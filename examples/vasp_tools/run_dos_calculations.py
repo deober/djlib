46082e3af764e42717e2f699f62abe22ef17417d
Jonathan Li <jonathan_li@ucsb.edu>
2022-04-18 17:57:52 -0700

    Functional auto_dos calculations + example usage part 2

--- examples/vasp_tools/run_dos_calculations.py
+++ examples/vasp_tools/run_dos_calculations.py
@@ -1,5 +1,12 @@
 from djlib import mc
-import os, sys
+import os
+
+"""
+For reference, the structure inside HfN_full/ground_states is 
+2 lists of configs FCC_gs.txt and HCP_gs.txt and 2 folders FCC and HCP
+each of FCC and HCP are structured as the training_data folder in CASM, with only the ground state configs
+ex. HfN_full/ground_states/FCC/SCEL1_1_1_1_0_0_0/1/calctype.default/ is a path
+"""
 
 root_dir = '/home/jonnyli/pod_experiments/HfN_full/ground_states'
 xtals = ['FCC','HCP']
