0d716f25eb3901ea7874aeb98d6826f95be62152
Jonathan Li <jli5373@gmail.com>
2022-04-27 20:22:29 -0700

    updated ulimit part to slurm submit script to avoid errors

--- templates/single_task_slurm_template.sh
+++ templates/single_task_slurm_template.sh
@@ -6,6 +6,7 @@
 #SBATCH --time={hours}:00:00              # Time limit hrs:min:sec
 #SBATCH --output={rundir}/slurmjob_%j.log # Standard output and error log
 
+ulimit -s unlimited
 cd {rundir}
 
 echo 'submitting from: ' {rundir}
