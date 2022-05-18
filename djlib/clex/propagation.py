727a4b8e2c900bf1296f844a44ca53e19998ee2f
deober <derick.ober@gmail.com>
2022-05-17 17:56:31 -0700

    Added collect ground state indices funciton.

--- djlib/clex/propagation.py
+++ djlib/clex/propagation.py
@@ -1,18 +1,17 @@
-import djlib.djlib as dj
+from lib2to3.pytree import convert
 import numpy as np
-import os
+from scipy.spatial import ConvexHull
+import thermofitting as tfit
 
 
-def collect_ground_state_indices(corr:np.ndarray, comp:np.ndarray, true_ground_state_indices:np.ndarray, predicted_energies:np.ndarray):
+def collect_ground_state_indices(
+    comp: np.ndarray, predicted_energies: np.ndarray,
+) -> list:
     """Stores ground state indices for each set of ECI. 
     Parameters:
     -----------
-    corr: np.ndarray
-        nxk Correlation matrix from casm query: n = number of configurations, k = number of ECIs
     comp: np.ndarray
         nxm Composition matrix from casm query: n = number of configurations, m = number of composition axes
-    true_ground_state_indices: np.ndarray
-        Vector of "true" ground state configurations.
     predicted_energies: np.ndarray
         sxn matrix of n predicted energies for each of s sets of ECI. 
     Returns:
@@ -22,4 +21,9 @@ def collect_ground_state_indices(corr:np.ndarray, comp:np.ndarray, true_ground_s
     """
     ground_state_indices = []
     for index, energy_set in enumerate(predicted_energies):
-        
+        points = np.hstack(comp, energy_set.reshape(-1, 1))
+        hull = ConvexHull(points)
+        hull_vertices, hull_simplices = tfit.hull.lower_hull(hull)
+        ground_state_indices.append(hull_vertices)
+    return ground_state_indices
+
