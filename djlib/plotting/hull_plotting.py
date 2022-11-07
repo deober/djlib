09d7403cde5c85197de3c309833a394210835593
deober <derick.ober@gmail.com>
2022-11-07 15:07:47 -0800

    Added a chemical potential window plotting function.

--- djlib/plotting/hull_plotting.py
+++ djlib/plotting/hull_plotting.py
@@ -5,6 +5,7 @@ from scipy.spatial import ConvexHull
 import thermocore.geometry.hull as thull
 import djlib.djlib as dj
 from sklearn.metrics import mean_squared_error
+import djlib.clex.clex as cl
 
 
 def general_binary_convex_hull_plotter(
@@ -86,3 +87,55 @@ def general_binary_convex_hull_plotter(
     fig = plt.gcf()
     fig.set_size_inches(19, 14)
     return fig
+
+
+def plot_stable_chemical_potential_windows_for_binary(
+    compositions: np.ndarray, energies: np.ndarray, names: np.ndarray
+) -> matplotlib.figure.Figure:
+    """Takes compositions, formation energies and names of elements and plots the stable chemical potential windows for a binary system.
+
+    Parameters
+    ----------
+    compositions: numpy.ndarray
+        Vector of compositions. Assumes a binary system, so compositions must be a 1D vector.
+    energies: numpy.ndarray
+        Vector of formation energies. Must be the same length as compositions.
+    names: numpy.ndarray
+        Optional, None by default. If provided, must be a 1D vector of the same length as compositions. Names are used to label the plot.
+    
+    Returns
+    -------
+    matplotlib.figure.Figure
+        Figure object containing the plot.
+    """
+
+    # Get the convex hull
+    hull = thull.full_hull(compositions, energies)
+    lower_hull_vertices = thull.lower_hull(hull)[0]
+
+    slopes = cl.calculate_slopes(
+        compositions[lower_hull_vertices], energies[lower_hull_vertices]
+    )
+
+    slope_windows = slope_windows = np.array(
+        [[slopes[i], slopes[i + 1]] for i in range(len(slopes) - 1)]
+    )
+
+    sorting_indices = np.argsort(np.ravel(compositions[lower_hull_vertices]))[1:-1]
+    lower_hull_vertices = lower_hull_vertices[sorting_indices]
+
+    for index, element in enumerate(lower_hull_vertices):
+        plt.plot(
+            np.ravel([compositions[element], compositions[element]]),
+            np.ravel(slope_windows[index]),
+            marker="o",
+            markersize=15,
+            label=names[element],
+        )
+    plt.legend(fontsize=21)
+
+    plt.xlabel("Composition X", fontsize=21)
+    plt.ylabel("Chemical Potential (eV)", fontsize=21)
+
+    plt.xticks(fontsize=21)
+    plt.yticks(fontsize=21)
