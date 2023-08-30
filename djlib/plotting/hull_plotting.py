f1908dbbd1729a2a9859fbd2fc54381521655fdc
Jonathan Li <jonathan_li@ucsb.edu>
2023-08-29 19:31:58 -0700

    Updated general_binary_convex_hull_plotter to take ax instead of just returning a fig. May break notebooks that use the old version

--- djlib/plotting/hull_plotting.py
+++ djlib/plotting/hull_plotting.py
@@ -9,26 +9,34 @@ import djlib.clex.clex as cl
 
 
 def general_binary_convex_hull_plotter(
+    ax: matplotlib.axes.Axes,
     composition: np.ndarray,
     true_energies: np.ndarray,
     predicted_energies=[None],
     print_extra_info: bool = False,
-) -> matplotlib.figure.Figure:
+) -> matplotlib.axes.Axes:
     """Plots a 2D convex hull for any 2D dataset. Can optionally include predicted energies to compare true and predicted formation energies and conved hulls.
 
     Parameters:
     -----------
+    ax: matplotlib.axes.Axes
+        The matplotlib axes object to plot on.
     composition: numpy.ndarray
         Vector of composition values, same length as true_energies.
     true_energies: numpy.ndarray
         Vector of formation energies. Required.
     predicted_energies: numpy.ndarray
         None by default. If a vector of energies are provided, it must be the same length as composition. RMSE score will be reported.
+
+    Returns:
+    --------    
+    matplotlib.axes.Axes
+        The matplotlib axes object that was passed in, updated with convex hull plot.
     """
 
     predicted_color = "red"
     predicted_label = "Predicted Energies"
-    plt.scatter(
+    ax.scatter(
         composition, true_energies, color="k", marker="1", label='"True" Energies'
     )
     dft_hull = thull.full_hull(compositions=composition, energies=true_energies)
@@ -47,7 +55,7 @@ def general_binary_convex_hull_plotter(
         spurious = np.setdiff1d(predicted_lower_hull_vertices, dft_lower_hull_vertices)
 
         if len(spurious) > 0:
-            plt.scatter(
+            ax.scatter(
                 composition[spurious],
                 predicted_energies[spurious],
                 color="royalblue",
@@ -76,7 +84,7 @@ def general_binary_convex_hull_plotter(
         for index in dft_lower_hull_vertices:
             print(index, composition[index])
     if any(predicted_energies):
-        plt.plot(
+        ax.plot(
             predicted_lower_hull[:, 0],
             predicted_lower_hull[:, 1],
             marker="D",
@@ -89,7 +97,7 @@ def general_binary_convex_hull_plotter(
             for index in predicted_lower_hull_vertices:
                 print(index, composition[index])
     if any(predicted_energies):
-        plt.scatter(
+        ax.scatter(
             composition,
             predicted_energies,
             color="red",
@@ -98,22 +106,19 @@ def general_binary_convex_hull_plotter(
         )
 
         rmse = np.sqrt(mean_squared_error(true_energies, predicted_energies))
-        plt.text(
+        ax.text(
             min(composition),
             0.9 * min(np.concatenate((true_energies, predicted_energies))),
-            "RMSE: " + str(rmse) + " eV",
+            "RMSE: " + str(rmse) + " eV/prim",
             fontsize=19,
         )
 
-    plt.xlabel("Composition X", fontsize=21)
-    plt.ylabel("Formation Energy per Primitive Cell (eV/prim)", fontsize=21)
     plt.legend(fontsize=19, loc="upper right")
-    plt.xticks(fontsize=18)
-    plt.yticks(fontsize=18)
-
-    fig = plt.gcf()
-    fig.set_size_inches(15, 12)
-    return fig
+    ax.set_xlabel('Composition X', fontsize=21)
+    ax.set_ylabel("Formation Energy per Primitive Cell (eV/prim)", fontsize=21)
+    ax.tick_params(axis='both', which='major', labelsize=18)
+    
+    return ax
 
 def binary_convex_hull_plotter_dft_and_overenumeration(ax, dft_comp, dft_formation_energies, dft_corr, over_comp, over_formation_energies, over_corr, dft_names=None, over_names=None, verbose=False):
     '''
